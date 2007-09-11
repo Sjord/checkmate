df021883f67d4dce7f4c7baf51e0760c10340fe8
sjoerd <sjoerd@f5a0e3f6-4f0a-0410-a315-d1dc7e33e6ef>
2007-09-11 14:35:24 +0000

    Added frame_print for debug purposes
    git-svn-id: http://svn.linuxonly.nl/public/checkmate/trunk@174 f5a0e3f6-4f0a-0410-a315-d1dc7e33e6ef

--- mpck/frame.c
+++ mpck/frame.c
@@ -61,3 +61,25 @@ frame_clear(frame)
 {
 	memset(frame, 0, sizeof(frame_info));
 }
+
+void
+frame_print(fi)
+        const frame_info *fi;
+{
+        printf("FRAME");
+        printf("    %-30s%d\n", "version", (fi->version));
+        printf("    %-30s%d\n", "layer", fi->layer);
+        printf("    %-30s%d bps\n", "bitrate", fi->bitrate);
+        printf("    %-30s%d Hz\n", "samplerate", fi->samplerate);
+        printf("    %-30s%d b\n", "frame_length", fi->length);
+        printf("    %-30s%d b (0x%x)\n", "offset", fi->offset, fi->offset);
+        printf("    %-30s%d ms\n", "time", fi->time);
+        printf("    %-30s%u\n", "crc16", fi->crc16);
+	printf("    %-30s%d\n", "padding", fi->padding);
+        printf("    %-30s%d\n", "stereo", fi->stereo);
+        printf("    %-30s%d\n", "joint stereo", fi->jointstereo);
+        printf("    %-30s%d\n", "copyright", fi->copyright);
+        printf("    %-30s%d\n", "emphasis", fi->emphasis);
+        printf("    %-30s%d\n", "original", fi->original);
+        printf("    %-30s%d\n", "samples", fi->samples);
+}
