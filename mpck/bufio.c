298af7f0b5aa2e3f465a0cd69c8c61284e768863
sjoerd <sjoerd@f5a0e3f6-4f0a-0410-a315-d1dc7e33e6ef>
2008-10-15 14:01:35 +0000

    Implemented io_funcs struct.
    git-svn-id: http://svn.linuxonly.nl/public/checkmate/trunk@219 f5a0e3f6-4f0a-0410-a315-d1dc7e33e6ef

--- mpck/bufio.c
+++ mpck/bufio.c
@@ -43,8 +43,33 @@
 #include <malloc.h>
 #endif
 
-#define USE_BUFFERED_IO
-#ifdef USE_BUFFERED_IO
+size_t ub_fread(char *outbuf, size_t size, CFILE *c);
+int ub_fseek(CFILE *c, long offset, int whence);
+size_t ub_ftell(CFILE *c);
+int ub_feof(CFILE *c);
+int ub_fclose(CFILE *c);
+
+size_t wb_fread(char *outbuf, size_t size, CFILE *c);
+int wb_fseek(CFILE *c, long offset, int whence);
+size_t wb_ftell(CFILE *c);
+int wb_feof(CFILE *c);
+int wb_fclose(CFILE *c);
+
+static io_funcs unbuffered_funcs = {
+	ub_fread,
+	ub_fseek,
+	ub_ftell,
+	ub_feof,
+	ub_fclose
+};
+
+static io_funcs buffered_funcs = {
+	wb_fread,
+	wb_fseek,
+	wb_ftell,
+	wb_feof,
+	wb_fclose
+};
 
 #define reminbuf(c) (size_t)(c->bufend - c->bufpnt)
 
@@ -56,8 +81,6 @@
 #ifndef S_ISREG
 #define S_ISREG(mode) (((mode) & S_IFREG)==(S_IFREG))
 #endif /* S_ISREG */
-
-static int buffered;
 
 static size_t _cfilesize(const char * filename) {
 	struct stat _ss;
@@ -89,6 +112,7 @@ CFILE * cfopen(const char * filename, char * mode) {
 	size_t filesize;
 	CFILE *c;
 	size_t nread;
+	int buffered;
 	
 	filesize=_cfilesize(filename);
 	if (filesize == -1) return NULL;
@@ -109,8 +133,12 @@ CFILE * cfopen(const char * filename, char * mode) {
 		return NULL;
 	}
 	c->filesize=filesize;
-	if (!buffered) return c;
+	if (!buffered) {
+		c->io_funcs = & unbuffered_funcs;
+		return c;
+	}
 
+	c->io_funcs = & buffered_funcs;
 	c->buffer=(char *)(c+1);
 	c->bufpnt=c->buffer;
 	c->buflen=filesize;
@@ -121,103 +149,93 @@ CFILE * cfopen(const char * filename, char * mode) {
 	return c;
 }
 
+/*
 size_t cfread(char * outbuf, size_t size, CFILE * c) {
-	if (buffered) {
-		size_t readnow;
-		readnow=MIN(reminbuf(c), size); 
-		// Most of the time, size is small and we do a fast assignment.
-		// This means we read beyond the end of the buffer. Hope it doesn't matter.
-		if (size <= sizeof(int)) {
-			* (int *)outbuf = * (int *)c->bufpnt;
-		} else {
-			memcpy(outbuf, c->bufpnt, readnow);
-		}
-		c->bufpnt += readnow;
-		return readnow;
-	} else {
-		return fread(outbuf, 1, size, c->fp);
-	}
-}
-
-int cfsetpos(CFILE * c, size_t pos) {
-	if (buffered) {
-		if (pos>c->buflen) return -1;
-		c->bufpnt=c->buffer+pos;
-		return 0;
-	} else {
-		return fseek(c->fp, pos, SEEK_SET);
-	}
+	return c->io_funcs->fread(outbuf, size, c);
 }
 
 int cfseek(CFILE * c, long offset, int whence) {
-	if (buffered) {
-		if (whence == SEEK_SET) return cfsetpos(c, offset);
-		if (whence == SEEK_CUR) {
-			return cfsetpos(c, cftell(c)+offset);
-		}
-		if (whence == SEEK_END) {
-			return cfsetpos(c, c->buflen+offset);
-		}
-		return -1;
-	} else {
-		return fseek(c->fp, offset, whence);
-	}
+	return c->io_funcs->fseek(c, offset, whence);
 }
 
 size_t cftell(CFILE * c) {
-	if (buffered) {
-		return c->bufpnt-c->buffer;
-	} else {
-		return ftell(c->fp);
-	}
+	return c->io_funcs->ftell(c);
 }
 
 int cfeof(CFILE * c) {
-	if (buffered) {
-		return reminbuf(c) == 0;
-	} else {
-		return feof(c->fp);
-	}
+	return c->io_funcs->feof(c);
 }
 
 int cfclose(CFILE * c) {
+	return c->io_funcs->fclose(c);
+}
+*/
+
+// whole buffered
+
+size_t wb_fread(char * outbuf, size_t size, CFILE * c) {
+	size_t readnow;
+	readnow=MIN(reminbuf(c), size); 
+	// Most of the time, size is small and we do a fast assignment.
+	// This means we read beyond the end of the buffer. Hope it doesn't matter.
+	if (size <= sizeof(int)) {
+		* (int *)outbuf = * (int *)c->bufpnt;
+	} else {
+		memcpy(outbuf, c->bufpnt, readnow);
+	}
+	c->bufpnt += readnow;
+	return readnow;
+}
+
+static int wb_fsetpos(CFILE * c, size_t pos) {
+	if (pos>c->buflen) return -1;
+	c->bufpnt=c->buffer+pos;
+	return 0;
+}
+
+int wb_fseek(CFILE * c, long offset, int whence) {
+	if (whence == SEEK_SET) return wb_fsetpos(c, offset);
+	if (whence == SEEK_CUR) {
+		return wb_fsetpos(c, cftell(c)+offset);
+	}
+	if (whence == SEEK_END) {
+		return wb_fsetpos(c, c->buflen+offset);
+	}
+	return -1;
+}
+
+size_t wb_ftell(CFILE * c) {
+	return c->bufpnt-c->buffer;
+}
+
+int wb_feof(CFILE * c) {
+	return reminbuf(c) == 0;
+}
+
+int wb_fclose(CFILE * c) {
 	int res=TRUE;
 	if (c->fp != NULL) res=fclose(c->fp);
 	free(c);
 	return res;
 }
 
-#else /* not USE_BUFFERED_IO */
-
-CFILE * cfopen(const char * filename, char * mode) {
-	CFILE *c=(CFILE *)malloc(sizeof(CFILE));
-	if (c == NULL) return NULL;
-	c->fp=fopen(filename, "rb");
-	if (c->fp == NULL) {
-		free(c);
-		return NULL;
-	}
-	return c;
-}
-
-size_t cfread(char * outbuf, size_t size, CFILE * c) {
+// unbuffered
+size_t ub_fread(char * outbuf, size_t size, CFILE * c) {
 	return fread(outbuf, 1, size, c->fp);
 }
 
-int cfseek(CFILE * c, long offset, int whence) {
+int ub_fseek(CFILE * c, long offset, int whence) {
 	return fseek(c->fp, offset, whence);
 }
 
-long cftell(CFILE * c) {
+long ub_ftell(CFILE * c) {
 	return ftell(c->fp);
 }
 
-int cfeof(CFILE * c) {
+int ub_feof(CFILE * c) {
 	return feof(c->fp);
 }
 
-int cfclose(CFILE * c) {
+int ub_fclose(CFILE * c) {
 	return fclose(c->fp);
 }
-
-#endif /* USE_BUFFERED_IO */
--- mpck/bufio.h
+++ mpck/bufio.h
@@ -13,6 +13,10 @@
 
 #define MIN(a, b) (a < b ? a : b)
 
+#define BUFFER_LIMIT 10000000 /* the maximum size of the buffer used by the buffered I/O */
+
+typedef struct _io_funcs io_funcs;
+
 typedef struct {
 	FILE * fp;
 	char * buffer;
@@ -20,6 +24,7 @@ typedef struct {
 	char * bufend;
 	size_t buflen;
 	size_t filesize;
+	io_funcs * io_funcs;
 } CFILE;
 
 /* bufio.c */
@@ -32,5 +37,19 @@ int cfseek(CFILE *c, long offset, int whence);
 size_t cftell(CFILE *c);
 int cfeof(CFILE *c);
 int cfclose(CFILE *c);
+
+struct _io_funcs {
+	size_t (*fread)(char *outbuf, size_t size, CFILE *c);
+	int (*fseek)(CFILE *c, long offset, int whence);
+	size_t (*ftell)(CFILE *c);
+	int (*feof)(CFILE *c);
+	int (*fclose)(CFILE *c);
+};
+
+#define cfread(outbuf, size, c) c->io_funcs->fread(outbuf, size, c)
+#define cfseek(c, offset, whence) c->io_funcs->fseek(c, offset, whence)
+#define cftell(c) c->io_funcs->ftell(c)
+#define cfeof(c) c->io_funcs->feof(c)
+#define cfclose(c) c->io_funcs->fclose(c)
 
 #endif
